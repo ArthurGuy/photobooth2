d2a528a058fcd1058aa8b4bc56b9fb4384307856
Arthur Guy <arthur@assys.net>
2019-06-27 11:24:00 +0100

    refactor/tidy

--- config.py
+++
@@ -1,8 +0,0 @@
-
-#Config settings to change behavior of photo booth
-monitor_w = 1024    # width of the display monitor
-monitor_h = 768    # height of the display monitor
-file_path = '/home/pi/Pictures/' # path to save images
-clear_on_startup = False # True will clear previously stored photos as the program launches. False will leave all previous photos.
-debounce = 0.3 # how long to debounce the button. Add more time if the button triggers too many times.
-post_online = False # True to upload images. False to store locally only.
--- photobooth.py
+++ photobooth.py
@@ -6,33 +6,35 @@ import time
 import traceback
 from time import sleep
 import RPi.GPIO as GPIO
-import picamera # http://picamera.readthedocs.org/en/release-1.4/install2.html
+import picamera  # http://picamera.readthedocs.org/en/release-1.4/install2.html
 import atexit
 import sys
 import socket
 import pygame
-import config # this is the config python file config.py
 from signal import alarm, signal, SIGALRM, SIGKILL
 import PIL.Image
 import gphoto2 as gp
 
-########################
-### Variables Config ###
-########################
-led_pin = 7 # LED 
-btn_pin = 18 # pin for the start button
+####################
+# Variables Config #
+####################
+button_led_pin = 7  # LED
+button_pin = 18  # pin for the start button
 
-num_pics_to_take = 3 # number of pics to be taken
-countdown_seconds = 4
-capture_delay = 3 # delay between pics
-prep_delay = 3 # number of seconds before step 1, after button press before countdown
-gif_delay = 20 # How much time between frames in the animated gif
-time_to_display_final_image = 5 # How long should the final combined image display for
+file_path = '/home/pi/Pictures/'  # path to save images
 
-#monitor_w = 1920    # width of the display monitor
-#monitor_h = 1080    # height of the display monitor
-monitor_w = 1024    # width of the display monitor
-monitor_h = 768    # height of the display monitor
+num_pics_to_take = 3  # number of pics to be taken
+countdown_seconds = 4  # On screen visual countdown
+capture_delay = 3  # delay between pics
+prep_delay = 3  # number of seconds before step 1, after button press before countdown
+gif_delay = 20  # How much time between frames in the animated gif
+time_to_display_final_image = 5  # How long should the final combined image display for
+time_to_display_finished_screen = 4  # The final finished graphic should display for this long
+
+# widescreen monitor 1920 x 1080
+# small booth monitor 1024 x 768
+monitor_w = 1024
+monitor_h = 768
 
 # Image ratio 4 x 3
 
@@ -47,13 +49,14 @@ high_res_w = 3280  # width of high res image, if taken
 high_res_h = 2464  # height of high res image, if taken
 
 make_gifs = True    # True to make an animated gif. False to post 4 jpgs into one post.
+make_photo_grid_image = True
 camera_iso = 400    # adjust for lighting issues. Normal is 100 or 200. Sort of dark is 400. Dark is 800 max.
-                    # available options: 100, 200, 320, 400, 500, 640, 800
+					# available options: 100, 200, 320, 400, 500, 640, 800
 
 
-#############################
-### Variables that Change ###
-#############################
+#########################
+# Variables that Change #
+#########################
 # Do not change these variables, as the code will change it anyway
 transform_x = monitor_w  # how wide to scale the jpg when replaying
 transfrom_y = monitor_h  # how high to scale the jpg when replaying
@@ -64,16 +67,17 @@ replay_cycles = 1  # how many times to show each photo on-screen after taking
 slr_camera = 0  # Are we using a proper camera to take photos
 
 
-####################
-### Other Config ###
-####################
+##################
+# Config & Setup #
+##################
 real_path = os.path.dirname(os.path.realpath(__file__))
 
 # GPIO setup
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led_pin, GPIO.OUT)  # LED
-GPIO.setup(btn_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.output(led_pin, False)  # for some reason the pin turns on at the beginning of the program. Why?
+GPIO.setup(button_led_pin, GPIO.OUT)
+GPIO.output(button_led_pin, False)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 # initialize pygame
 pygame.init()
@@ -86,18 +90,21 @@ pygame.display.toggle_fullscreen()
 # Load the background template
 bgimage = PIL.Image.open(real_path + "/background.png")
 
-# Setup the gphoto camera connection
-context = gp.Context()
-camera = gp.Camera()
-try:
-	camera.init(context)
-	slr_camera = 1
-except gp.GPhoto2Error as ex:
-	slr_camera = 0
 
-#################
-### Functions ###
-#################
+#############
+# Functions #
+#############
+
+
+def setup_slr_camera():
+	global slr_camera
+	context = gp.Context()
+	camera = gp.Camera()
+	try:
+		camera.init(context)
+		slr_camera = 1
+	except gp.GPhoto2Error as ex:
+		slr_camera = 0
 
 
 # clean up running programs as needed when main program exits
@@ -113,8 +120,7 @@ atexit.register(cleanup)
 # A function to handle keyboard/mouse/device input events    
 def input(events):
 	for event in events:  # Hit the ESC key to quit the slideshow.
-		if (event.type == pygame.QUIT or
-			(event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE)):
+		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
 			pygame.quit()
 
 
@@ -175,16 +181,16 @@ def clear_screen():
 
 # display a group of images
 def display_pics(base_file_name):
-    for i in range(0, replay_cycles): #show pics a few times
+	for i in range(0, replay_cycles): #show pics a few times
 		for i in range(1, num_pics_to_take+1): #show each pic
-			show_image(config.file_path + base_file_name + "-" + str(i) + ".jpg")
+			show_image(file_path + base_file_name + "-" + str(i) + ".jpg")
 			time.sleep(replay_delay) # pause
 
 
 def combine_pics(base_file_name):
 	try:
 		for i in range(1, num_pics_to_take+1):
-			image = PIL.Image.open(config.file_path + base_file_name + "-" + str(i) + "-sm.jpg")
+			image = PIL.Image.open(file_path + base_file_name + "-" + str(i) + "-sm.jpg")
 			if i == 1:
 				bgimage.paste(image, (25, 25))
 			if i == 2:
@@ -194,7 +200,7 @@ def combine_pics(base_file_name):
 			if i == 4:
 				bgimage.paste(image, (650, 500))
 
-		filename = config.file_path + base_file_name + '-combined.jpg'
+		filename = file_path + base_file_name + '-combined.jpg'
 		bgimage.save(filename)
 		return filename
 	except Exception, e:
@@ -223,15 +229,20 @@ def display_countdown_number(number):
 	pygame.display.flip()
 
 
+# Capture images from both cameras and display
+def start_image_test():
+	print "Testing cameras"
+
+
 # define the photo taking function for when the big button is pressed 
 def start_photobooth(): 
 
-	input(pygame.event.get()) # press escape to exit pygame. Then press ctrl-c to exit python.
+	# input(pygame.event.get())
 
-	################################# Begin Step 1 #################################
+	# Display the instructions screen and prep the camera
 	
 	print "Get Ready"
-	GPIO.output(led_pin,False);
+	GPIO.output(button_led_pin, False)
 	show_image(real_path + "/instructions.png")
 	sleep(prep_delay)
 	
@@ -240,62 +251,56 @@ def start_photobooth():
 	camera = picamera.PiCamera(sensor_mode=2)
 	camera.vflip = False
 	camera.hflip = True # flip for preview, showing users a mirror image
-	#camera.saturation = -100 # comment out this line if you want color images
 	camera.iso = camera_iso
-	
-	#pixel_width = 0 # local variable declaration
-	#pixel_height = 0 # local variable declaration
-	
+
+	# Preview screen should be centered on the screen
 	preview_window_x = (monitor_w - preview_image_w) / 2
 	preview_window_y = (monitor_h - preview_image_h) / 2
-	
-	
 		
-	################################# Take the photos #################################
+	# Take the photos
 	
 	print "Taking pics"
 	
-	base_file_name = time.strftime("%H-%M-%S")  # get the current date and time for the start of the filename
-	
+	base_file_name = time.strftime("%H-%M-%S")  # get the current time for the start of the filename
 
-	try: # take the photos
+	try:
 		for i in range(1, num_pics_to_take+1):
 			camera.hflip = True  # preview a mirror image
 			
-			#display_header_text("Get ready")
+			# display_header_text("Get ready")
 			
 			# Turn on the camera preview overlay
 			camera.resolution = (preview_image_w, preview_image_h)
 			camera.start_preview(fullscreen=False, window=(preview_window_x, preview_window_y, preview_image_w, preview_image_h))
+			# Semi transparent image so the countdown text shows through
 			camera.preview.alpha = 200
-			
+
+			# Display the countdown on screen
 			for countdown in range(countdown_seconds, 0, -1):
 				display_countdown_number(countdown)
 				time.sleep(1)
 				clear_screen()
 			
-			GPIO.output(led_pin, True)  # turn on the LED
-			
 			camera.stop_preview()
-			
+
+			# Flash the screen white to simulate the image being taken
 			screen.fill(pygame.Color("white"))
 			pygame.display.flip()
-			
-			filename = config.file_path + base_file_name + '-' + str(i) + '.jpg'
-			camera.hflip = False  # flip back when taking photo
+
+			# reset the camera to full res and flip the image before taking a shot
+			camera.hflip = False
 			camera.resolution = (high_res_w, high_res_h)
+			filename = file_path + base_file_name + '-' + str(i) + '.jpg'
 			camera.capture(filename)
-			#camera.capture(filename, resize=(high_res_w, high_res_h))
 			print(filename)
-			
+
+			# Go back to a black screen
 			screen.fill(pygame.Color("black"))
 			pygame.display.flip()
 			
-			GPIO.output(led_pin, False)  # turn off the LED
-			
-			#show_image(real_path + "/pose" + str(i) + ".png")
-			#show_image(filename)
-			#time.sleep(capture_delay) # pause in-between shots
+			# show_image(real_path + "/pose" + str(i) + ".png")
+			# show_image(filename)
+			# time.sleep(capture_delay) # pause in-between shots
 			
 			clear_screen()
 			
@@ -311,70 +316,63 @@ def start_photobooth():
 	finally:
 		camera.close()
 
-		
-	########################### Produce the combined images #################################
+	# Produce the combined images
 	
-	input(pygame.event.get()) # press escape to exit pygame. Then press ctrl-c to exit python.
+	# input(pygame.event.get())
 	
 	print "Creating an animated gif" 
 	
 	show_image(real_path + "/processing.png")
 	
 	# Make a small version of the images
-	for x in range(1, num_pics_to_take+1):  # batch process all the images
-		graphicsmagick = "gm convert -size 600x450 " + config.file_path + base_file_name + "-" + str(x) + ".jpg -thumbnail 600x450 " + config.file_path + base_file_name + "-" + str(x) + "-sm.jpg"
-		os.system(graphicsmagick)  # do the graphicsmagick action
+	for x in range(1, num_pics_to_take + 1):  # batch process all the images
+		graphicsmagick = "gm convert -size 600x450 " + file_path + base_file_name + "-" + str(x) + ".jpg -thumbnail 600x450 " + file_path + base_file_name + "-" + str(x) + "-sm.jpg"
+		os.system(graphicsmagick)
 	
 	# Allow a moment for the small images to create before we use them
-	time.sleep(1)
+	# time.sleep(1)
 				
-	if make_gifs: # make the gifs
-		graphicsmagick = "gm convert -delay " + str(gif_delay) + " " + config.file_path + base_file_name + "-*-sm.jpg " + config.file_path + base_file_name + ".gif" 
-		os.system(graphicsmagick)  # make the .gif
+	if make_gifs:
+		graphicsmagick = "gm convert -delay " + str(gif_delay) + " " + file_path + base_file_name + "-*-sm.jpg " + file_path + base_file_name + ".gif"
+		os.system(graphicsmagick)
 
-	# Combine the images into a grid image
-	filename = combine_pics(base_file_name)
-	show_image(filename)
-	time.sleep(time_to_display_final_image)
+	if make_photo_grid_image:
+		filename = combine_pics(base_file_name)
+		show_image(filename)
+		time.sleep(time_to_display_final_image)
 	
 	# Delete the small images
 	try:
 		for x in range(1, num_pics_to_take + 1):
-			os.remove(config.file_path + base_file_name + "-" + str(x) + "-sm.jpg")
+			os.remove(file_path + base_file_name + "-" + str(x) + "-sm.jpg")
 	except Exception, e:
-		print "Error deleting thmbnails"
+		print "Error deleting thumbnails"
 		tb = sys.exc_info()[2]
 		traceback.print_exception(e.__class__, e, tb)
 		
-	########################### Finished #################################
+	# Finished
 	
-	input(pygame.event.get())  # press escape to exit pygame. Then press ctrl-c to exit python.
-	
-	#try:
-	#	display_pics(base_file_name)
-	#except Exception, e:
-	#	tb = sys.exc_info()[2]
-	#	traceback.print_exception(e.__class__, e, tb)
-	#	pygame.quit()
-		
+	# input(pygame.event.get())
+
+	# display_pics(base_file_name)
+
+	if time_to_display_finished_screen > 0:
+		show_image(real_path + "/finished.png")
+		time.sleep(time_to_display_finished_screen)
+
 	print "Done"
 	
-	# show_image(real_path + "/finished2.png")
-	
 	show_image(real_path + "/intro.png")
-	GPIO.output(led_pin, True)  # turn on the LED
+	GPIO.output(button_led_pin, True)
 
 
 def wait_for_start():
-	global pygame
+	# global pygame
 	while True:
-		channel = GPIO.wait_for_edge(btn_pin, GPIO.FALLING, timeout=500)
+		channel = GPIO.wait_for_edge(button_pin, GPIO.FALLING, timeout=500)
 		if channel is not None:
 			# Button press
 			return
-	    	# input_state = GPIO.input(btn_pin)
-	    	# if input_state == False:
-		#	return
 		for event in pygame.event.get():			
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_ESCAPE:
@@ -383,32 +381,36 @@ def wait_for_start():
 					return
 		time.sleep(0.2)
 	
-####################
-### Main Program ###
-####################
+################
+# Main Program #
+################
 
 
-print "Photo booth app running..." 
+print "Photo booth app starting..."
+
+setup_slr_camera()
 
 if slr_camera:
 	print "SLR Camera connected"
 else:
-	print "NO SLR Canera connected"
+	print "NO SLR Camera connected"
 
 for x in range(0, 2):  # blink light to show the app is running
-	GPIO.output(led_pin, True)
+	GPIO.output(button_led_pin, True)
 	sleep(0.25)
-	GPIO.output(led_pin, False)
+	GPIO.output(button_led_pin, False)
 	sleep(0.25)
 
-show_image(real_path + "/intro.png");
+print "Photo booth app ready..."
+
+show_image(real_path + "/intro.png")
 
 while True:
-	GPIO.output(led_pin,True)  # turn on the light showing users they can push the button
-	input(pygame.event.get())  # press escape to exit pygame. Then press ctrl-c to exit python.
+	GPIO.output(button_led_pin, True)  # turn on the light showing users they can push the button
+	input(pygame.event.get())
 	
 	wait_for_start()
 	
-	# GPIO.wait_for_edge(btn_pin, GPIO.FALLING)
-	# time.sleep(config.debounce) #debounce
+	# GPIO.wait_for_edge(button_pin, GPIO.FALLING)
+	# time.sleep(0.3) #debounce
 	start_photobooth()
