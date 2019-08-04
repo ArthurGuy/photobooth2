e86db926bc5faf4a20293eb04214c40d51f23eba
Arthur Guy <arthur@assys.net>
2019-08-04 13:20:19 +0100

    print test

--- printer.py
+++ printer.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
 import cups
+from time import sleep
 
 conn = cups.Connection()
 printers = conn.getPrinters()
 for printer in printers:
     print printer, printers[printer]["device-uri"]
+
+printer_name = 'cp220'
+
+test_image = '/home/pi/photobooth/test.jpg'
+
+print_id = conn.printFile(printer_name, test_image, "Photo Booth", {})
+# Wait until the job finishes
+print 'Printing'
+while conn.getJobs().get(print_id, None):
+    print '.'
+    sleep(5)
+
+print 'Finished'
